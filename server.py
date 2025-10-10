a08ee4352988086559ade65b6d04920ff60d0745
defacto09 <155910215+defacto09@users.noreply.github.com>
2025-10-10 18:27:03 +0300

    v15 mb fix

--- server.py
+++ server.py
@@ -38,6 +38,8 @@ API_KEY = os.getenv("API_KEY")
 db_password_escaped = quote_plus(db_password)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_password_escaped}@{db_host}:{db_port}/{db_name}?charset=utf8mb4"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024  # 200 MB
+
 
 db = SQLAlchemy(app)
 
