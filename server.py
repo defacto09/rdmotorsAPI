d247d18999955c3600fd61687b790021587aedae
defacto09 <155910215+defacto09@users.noreply.github.com>
2025-10-21 18:45:40 +0300

    1.2.3 http requests (for testing by frontender)

--- server.py
+++ server.py
@@ -75,7 +75,7 @@ db_password_escaped = quote_plus(db_password)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_password_escaped}@{db_host}:{db_port}/{db_name}?charset=utf8mb4"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024  # 200 MB
-app.config['PREFERRED_URL_SCHEME'] = 'https'
+app.config['PREFERRED_URL_SCHEME'] = 'http'
 
 
 db = SQLAlchemy(app)
